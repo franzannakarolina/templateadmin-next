f17d446b7f8c1825ec938fd3713ca5198734b8b7
Anna Karolina Franz <63086797+franzannakarolina@users.noreply.github.com>
2022-05-06 23:27:03 -0300

    webpack4 no next

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  env: {
+    BASE_URL: process.env.BASE_URL,
+ },
+   reactStrictMode: true,
+   webpack5: false,
+   webpack4: (config, options) => {
+    config.module.rules.push({
+      test: /\.mdx/,
+      use: [
+        options.defaultLoaders.babel,
+        {
+          loader: '@mdx-js/loader',
+          options: pluginOptions.options,
+        },
+      ],
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'),
+      }
+    })
+
+    return config
+  },
+ }
